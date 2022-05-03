ee4668e686abad64afd0fd5efa787138d591d1a8
Facundo Sanchez <facuujos@gmail.com>
2022-05-03 20:01:36 -0300

    layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  // {path:'course',component:ProbandoComponent},
+  // {path:'admin',component:ProbandoComponent},
+  // {path:'student',component:ProbandoComponent},
+  // {path:'teacher',component:ProbandoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { AdminComponent } from './components/users/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/layout/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminComponent,
+    LoginComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
